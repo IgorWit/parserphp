4d9edc2bc61137578d3dc975d99b2e70736703a6
Igor Vitruk <vitruk.igor1@gmail.com>
2017-10-07 18:36:12 +0300

    third status

--- index.php
+++ index.php
@@ -5,5 +5,6 @@
 	<title>Parser PHP</title>
 </head>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, cupiditate, magnam. Dolores illum id sit at quis, voluptatem quisquam nulla veniam amet itaque nobis eius labore consectetur alias saepe maxime.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem modi porro fuga neque voluptatibus aliquam quaerat, excepturi culpa exercitationem doloremque possimus accusantium odit totam provident, consequuntur adipisci nisi perferendis voluptate.</p>
 </body>
 </html>
