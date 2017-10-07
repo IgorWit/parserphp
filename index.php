6c6a328c16904d3159beae4beb8aa36c2c48df33
Igor Vitruk <vitruk.igor1@gmail.com>
2017-10-07 18:35:23 +0300

    second commit

--- index.php
+++ index.php
@@ -4,10 +4,6 @@
 	<meta charset="UTF-8">
 	<title>Parser PHP</title>
 </head>
-<body>
-	<?
-	echo "Test PHP!";
-	?>
-	<h1>Git 2 </h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, cupiditate, magnam. Dolores illum id sit at quis, voluptatem quisquam nulla veniam amet itaque nobis eius labore consectetur alias saepe maxime.</p>
 </body>
 </html>
